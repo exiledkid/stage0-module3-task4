0ac2dcd9ec0fb93e19f0fe7b5ea91688675a7026
exiledkid <fortunemodspartner@gmail.com>
2022-12-17 22:00:08 +0300

    ...

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-//         first = 10_000_000_000L;
-//         second = 'a';
-//         third = 5 > 9;
-//         forth = 12.90434323324342;
-//         fifth = 12.3F;
-//         sixth = 1;
-//         seventh = 129;
+//         long = 10_000_000_000L;
+//         char = 'a';
+//         boolean = 5 > 9;
+//         double = 12.90434323324342;
+//         float = 12.3F;
+//         byte = 1;
+//         short = 129;
     }
 }
